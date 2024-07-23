6aedba32ae195bce99bd3ed891bb41bad6b9a354
XpieX <dzauqi8@gmail.com>
2024-07-23 14:58:11 +0700

    m

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loginButton").onclick = function () {
+        window.location.href = 'register.html';
+    };
+});
