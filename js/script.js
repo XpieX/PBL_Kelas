ff2520b58a67878eb2422a36861d4ea4ba4cc980
ArielClyDe <arielramadan99@gmail.com>
2024-07-24 11:52:26 +0700

    d

--- js/script.js
+++ js/script.js
@@ -49,3 +49,23 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+/* Tambahkan ke dalam js/script.js */
+
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Tutup dropdown jika pengguna mengklik di luar dropdown
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
